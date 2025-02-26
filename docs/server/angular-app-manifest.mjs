87a12be23ceeb6ac8b219b92736e487f5ba591aa
Kevin Radlowski <kevin.radlowski@gmail.com>
2025-02-26 10:06:40 +0100

    modification de la police

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 56888, hash: '35b663a123704da585237f5c0aecfbd5dbf02cf8c02068d9b220c3fa94c773dc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17188, hash: '69a3210e114e0dd7701d1943ce3277158465c9294a33cb914eaf3950e6d0ea12', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 105782, hash: '874f0481fc2a4326ba190e298813e9a6a668993e0b6df672d0a28be5085b81b9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-UQXLRT5L.css': {size: 315784, hash: 'lKsQOlLoEps', text: () => import('./assets-chunks/styles-UQXLRT5L_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 57992, hash: 'b69330b09b14e486fc77c4d0fd43fb9a63abbff5e38dd7650d1988ec19aa8fd8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 18292, hash: '3a7937b25383214ba6bd6b7950c3deb8eb5e16ff08d611fc491f358beea4548a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 106923, hash: '7cfbcc289b6aadba646c92c2ede93ade1446b2acd2970fa14f2d4532512cb48a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-ZHNIAXMZ.css': {size: 315824, hash: 'LUrjEnCaupQ', text: () => import('./assets-chunks/styles-ZHNIAXMZ_css.mjs').then(m => m.default)}
   },
 };
