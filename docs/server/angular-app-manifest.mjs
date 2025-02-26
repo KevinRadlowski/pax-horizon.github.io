fc49d07e272f6209cf46d8185d3be3b04ca9c829
Kevin Radlowski <kevin.radlowski@gmail.com>
2025-02-26 10:14:50 +0100

    Ajout map minecraft

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 57992, hash: 'b69330b09b14e486fc77c4d0fd43fb9a63abbff5e38dd7650d1988ec19aa8fd8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 18292, hash: '3a7937b25383214ba6bd6b7950c3deb8eb5e16ff08d611fc491f358beea4548a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 106923, hash: '7cfbcc289b6aadba646c92c2ede93ade1446b2acd2970fa14f2d4532512cb48a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 57992, hash: '45c0a24bfd62275503ceecd1a18279ec10de0e3f10a1c30b931a1f19eece5e68', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 18292, hash: '90635f5a4914f77f08d9d882b51c69471f9579b3bc7fab793ed3bcdc480ab332', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 108756, hash: '1d45b0749334900cadd1db530c5b6519cae6f4ca4444052e5d8d3b92b3a43c6f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-ZHNIAXMZ.css': {size: 315824, hash: 'LUrjEnCaupQ', text: () => import('./assets-chunks/styles-ZHNIAXMZ_css.mjs').then(m => m.default)}
   },
 };
